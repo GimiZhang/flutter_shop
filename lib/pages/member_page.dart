b372476b78ce56894b5c7647edc8e4f508d9a7c8
608564@hopson.com.cn <zf121318>
2019-09-26 11:31:52 +0800

    会员中心页面开发完毕

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -9,7 +9,12 @@ class MemberPage extends StatelessWidget {
         title: Text("会员中心"),
       ),
       body: ListView(
-        children: <Widget>[_topHeader()],
+        children: <Widget>[
+          _topHeader(),
+          _orderTitle(),
+          _orderList(),
+          _actionsList()
+        ],
       ),
     );
   }
@@ -40,6 +45,119 @@ class MemberPage extends StatelessWidget {
               style: TextStyle(fontSize: 15, color: Colors.black54),
             ),
           )
+        ],
+      ),
+    );
+  }
+
+  Widget _orderTitle() {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(bottom: BorderSide(width: 1, color: Colors.black12))),
+      child: ListTile(
+        leading: Icon(Icons.list),
+        title: Text("我的订单"),
+        trailing: Icon(Icons.chevron_right),
+      ),
+    );
+  }
+
+  Widget _orderList() {
+    return Container(
+      height: ScreenUtil().setHeight(240),
+      alignment: Alignment.center,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: ScreenUtil().setWidth(260),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.monetization_on),
+                SizedBox(
+                  height: 10,
+                ),
+                Text('待付款')
+              ],
+            ),
+          ),
+          Container(
+            width: ScreenUtil().setWidth(260),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.send),
+                SizedBox(
+                  height: 10,
+                ),
+                Text('待发货')
+              ],
+            ),
+          ),
+          Container(
+            width: ScreenUtil().setWidth(260),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.directions_car),
+                SizedBox(
+                  height: 10,
+                ),
+                Text('待收货')
+              ],
+            ),
+          ),
+          Container(
+            width: ScreenUtil().setWidth(260),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.comment),
+                SizedBox(
+                  height: 10,
+                ),
+                Text('待评价')
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _list(IconData icon, String title) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(bottom: BorderSide(width: 1, color: Colors.black12))),
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+        trailing: Icon(Icons.chevron_right),
+      ),
+    );
+  }
+
+  Widget _actionsList() {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: Column(
+        children: <Widget>[
+          _list(Icons.favorite_border, "领取优惠券"),
+          _list(Icons.favorite, "已领取优惠券"),
+          _list(Icons.location_on, "地址管理"),
+          SizedBox(
+            height: 10,
+          ),
+          _list(Icons.contact_phone, "客服电话"),
+          _list(Icons.store, "关于商城"),
         ],
       ),
     );
